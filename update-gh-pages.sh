cddfcaeb8eeea896f2ddea4a8808671ec8ef9834
Christopher Dembia <cld72@cornell.edu>
2013-10-09 01:36:48 -0700

    Creating separate clone for updating documentation.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,14 +4,16 @@
 echo -e "Starting to update gh-pages.\n"
 make doxygen
 cp -R html $HOME/html
-git checkout -b gh-pages origin/gh-pages
-rm -rf *
-mv $HOME/html/ .
+cd $HOME
 git config --global user.email "traivs@travis-ci.org"
 git config --global user.name "Travis"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/chrisdembia/simbody.git gh-pages > /dev/null
+cd gh-pages
+git rm -rf .
+cp -Rf $HOME/html/* .
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages."
-git push -fq https://github.com/chrisdembia/simbody.git gh-pages > /dev/null
+git push -fq origin gh-pages > /dev/null
 echo -e "Done updating gh-pages.\n"
 #fi
 
