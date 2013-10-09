c5cb12bfebc0e66793aea90cf8301c5a89807d0c
Christopher Dembia <cld72@cornell.edu>
2013-10-09 01:39:26 -0700

    Fix typo in travis email address.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -5,7 +5,7 @@ echo -e "Starting to update gh-pages.\n"
 make doxygen
 cp -R html $HOME/html
 cd $HOME
-git config --global user.email "traivs@travis-ci.org"
+git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/chrisdembia/simbody.git gh-pages > /dev/null
 cd gh-pages
